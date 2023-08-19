0d07633d89f51502b73134fa8f13053736f3f2de
9033 <28668509+9033@users.noreply.github.com>
2023-08-19 23:46:47 +0900

    install tester

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  // https://vitest.dev/guide/#configuring-vitest
+  test: {
+    // jest와 같은 전역 테스트 API 사용
+    globals: true,
+    // happy-dom으로 DOM 시뮬레이션
+    // (피어 의존성으로 happy-dom을 설치해야 함)
+    environment: "happy-dom",
+  },
+  // jest not found
+  // resolve: {
+  //   conditions: process.env.VITEST ? ["node"] : [],
+  // },
+});
