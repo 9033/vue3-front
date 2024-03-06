bf3ab0bbf36b48a2902a6ba8392dedf41bad3a38
9033 <28668509+9033@users.noreply.github.com>
2024-03-06 22:32:19 +0900

    update links

--- src/components/GenshinImpact.vue
+++ src/components/GenshinImpact.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { defineAsyncComponent } from "vue";
-import { Content } from "../types/embed.ts";
+import { Content } from "../types/embed";
+import { convertToContent } from "../script/util";
 
 const Embed = defineAsyncComponent(() => import("./Embed.vue"));
 
@@ -12,6 +13,8 @@ const tags: Array<string> = `
   Aether
   Paimon
   Kokomi
+  Neuvillette
+  Furina
 `
   .split(/\s+/)
   .filter((x) => !!x);
@@ -19,60 +22,80 @@ const tags: Array<string> = `
 const contents: Content[] = [
   {
     type: "youtube",
+    url: "https://www.youtube.com/embed/JjaszAiDuNk?si=yFUawwkbiVPaosj-",
+    tags: "Neuvillette",
+  },
+  {
+    type: "youtube",
+    url: "https://www.youtube.com/embed/WR671Bs9tH0?si=yBo9Cj9b_2LmGAX-",
+    tags: "Neuvillette Furina",
+  },
+  {
+    type: "youtube",
+    url: "https://www.youtube.com/embed/f1M0MF5u4lM?si=7AY1aKXO9phFBCK7",
+    tags: "Neuvillette",
+  },
+  {
+    type: "youtube",
+    url: "https://www.youtube.com/embed/pEBl2Rk60TA?si=kUmM4ybEhGGloFBM",
+    tags: "Neuvillette Furina Lumine",
+  },
+  {
+    type: "youtube",
     url: "https://www.youtube.com/embed/KaCXs79zdls",
-    tags: "Lumine Nahida".split(" "),
+    tags: "Lumine Nahida",
   },
   {
     type: "youtube",
     url: "https://www.youtube.com/embed/jON5ZozLStw",
-    tags: "Lumine Nahida".split(" "),
+    tags: "Lumine Nahida",
   },
   {
     type: "youtube",
     url: "https://www.youtube.com/embed/hffiEkX95wA",
-    tags: "Klee Kaeya".split(" "),
+    tags: "Klee Kaeya",
   },
   {
     type: "youtube",
     url: "https://www.youtube.com/embed/qhPv2m1iWI4",
-    tags: "Klee Kaeya".split(" "),
+    tags: "Klee Kaeya",
   },
   {
     type: "youtube",
     url: "https://www.youtube.com/embed/CG0GC8rTqqE",
-    tags: "Aether Nahida".split(" "),
+    tags: "Aether Nahida",
   },
   {
     type: "youtube",
     url: "https://www.youtube.com/embed/sMBB15x8s4c",
-    tags: "Klee Nahida".split(" "),
+    tags: "Klee Nahida",
   },
   {
     type: "youtube",
     url: "https://www.youtube.com/embed/3r84Om3ylA8",
-    tags: "Lumine Nahida".split(" "),
+    tags: "Lumine Nahida",
   },
   {
     type: "youtube",
     url: "https://www.youtube.com/embed/Lh7J9fDIGQk",
-    tags: "Klee Nahida".split(" "),
+    tags: "Klee Nahida",
   },
   {
     type: "youtube",
     url: "https://www.youtube.com/embed/6LxZt2mElhA",
-    tags: "Paimon Nahida".split(" "),
+    tags: "Paimon Nahida",
   },
   {
     type: "youtube",
     url: "https://www.youtube.com/embed/4y8rDczyY3w",
-    tags: "Kokomi Klee".split(" "),
+    tags: "Kokomi Klee",
   },
   {
     type: "youtube",
     url: "https://www.youtube.com/embed/aAeGYsUs5lo",
-    tags: "Lumine Nahida".split(" "),
+    tags: "Lumine Nahida",
   },
-];
+].map(convertToContent);
 </script>
 
 <template>
--- src/components/RealEstate.vue
+++ src/components/RealEstate.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { defineAsyncComponent } from "vue";
-import { Content } from "../types/embed.ts";
+import { Content } from "../types/embed";
+import { convertToContent } from "../script/util";
 
 const Embed = defineAsyncComponent(() => import("./Embed.vue"));
 
@@ -14,9 +15,9 @@ const contents: Content[] = [
   {
     type: "youtube",
     url: "https://www.youtube.com/embed/NRzBnto0A6I",
-    tags: "재개발".split(" "),
+    tags: "재개발",
   },
-];
+].map(convertToContent);
 </script>
 
 <template>
