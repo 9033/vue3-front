2f112276f33af7ddc449e0176b4a8750f848eae0
9033 <28668509+9033@users.noreply.github.com>
2024-04-10 06:04:46 +0900

    yaml - viewer form
    템플릿에 정의된 리소스를 선택

--- src/components/Yaml.vue
+++ src/components/Yaml.vue
@@ -22,8 +22,13 @@
           disabled
         >
           <option value="">--Please choose an option--</option>
-          <option value="OS::Nova::Server">OS::Nova::Server</option>
-          <option value="OS::Cinder::Volume">OS::Cinder::Volume</option>
+          <option
+            v-for="resourceType in resourceTypes"
+            :key="resourceType"
+            :value="resourceType"
+          >
+            {{ resourceType }}
+          </option>
         </select>
         <template
           v-for="property in Object.keys(
@@ -36,7 +41,12 @@
               v-for="_property in templateData?.resources?.[resource]
                 ?.properties?.[property] ?? []"
             >
-              <input type="string" disabled :value="_property?.network ?? ''" />
+              <Property
+                property="network"
+                :properties="_property"
+                :resources="resources"
+                disabled
+              />
             </template>
           </template>
           <template v-else-if="property === 'block_device_mapping'">
@@ -45,26 +55,22 @@
               v-for="_property in templateData?.resources?.[resource]
                 ?.properties?.[property] ?? []"
             >
-              <Property property="device_name" :properties="_property" />
+              <Property
+                property="device_name"
+                :properties="_property"
+                disabled
+              />
               <Property
                 property="delete_on_termination"
                 :properties="_property"
+                disabled
               />
-              <label>volume_id</label>
-              <template v-if="typeof _property?.volume_id === 'object'">
-                <input
-                  type="string"
-                  disabled
-                  :value="_property?.volume_id?.get_resource"
-                />
-              </template>
-              <template v-else>
-                <input
-                  type="string"
-                  disabled
-                  :value="_property?.volume_id ?? ''"
-                />
-              </template>
+              <Property
+                property="volume_id"
+                :properties="_property"
+                :resources="resources"
+                disabled
+              />
             </template>
           </template>
           <template v-else>
@@ -76,6 +82,8 @@
               "
               :property="property"
               :properties="templateData?.resources?.[resource]?.properties"
+              :resources="resources"
+              disabled
             />
           </template>
           <!-- <template
@@ -122,31 +130,24 @@
   </div>
 </template>
 <script setup lang="ts">
-import {
-  computed,
-  defineAsyncComponent,
-  defineComponent,
-  h,
-  onMounted,
-  ref,
-} from "vue";
-// import yaml from "js-yaml";
-import { yaml2obj, inputType } from "../script/yaml";
+import { computed, defineAsyncComponent, onMounted, ref } from "vue";
+import { yaml2obj } from "../script/yaml";
 
 const Property = defineAsyncComponent(() => import("./yaml/Property.vue"));
 
-const YAML2 = `heat_template_version: 2021-04-16
+/** heat template */
+const YAML = `heat_template_version: 2021-04-16
 
 resources:
   my_instance:
     type: OS::Nova::Server
     properties:
-      name: instance-1
-      image: image-1
+      name: ljk-test-instance
+      image: ubuntu-1
       flavor: flavor-1
       networks:
-        - network: network-1
-      key_name: ssh-1
+        - network: { get_resource: my_network}
+      key_name: ljk-ssh
       block_device_mapping: [
         {
           device_name: vda,
@@ -154,93 +155,66 @@ resources:
           delete_on_termination: true,
         },
       ]
-  
+
   my_volume:
     type: OS::Cinder::Volume
     properties:
-      name: volumn-1
-      size: 20
+      name: ljk-test-volume
+      size: 18
+      description: ljk's volume
+
+  my_network:
+    type: OS::Neutron::Net
+    properties:
+      name: ljk-test-network
+
+  my_subnet:
+    type: OS::Neutron::Subnet
+    properties:
+      name: ljk-test-subnet
+      network: { get_resource: my_network}
+      cidr: 192.168.201.0/24
+
+  my_router:
+    type: OS::Neutron::Router
+    properties:
+      name: ljk-test-router
+      external_gateway_info:
+        network: provider
+
+  my_router_interface:
+    type: OS::Neutron::RouterInterface
+    properties:
+      router: { get_resource: my_router}
+      subnet: { get_resource: my_subnet}
+
+  # my_port:
+  #   type: OS::Neutron::Port
+  #   properties:
+  #     name: ljk-test-port
+  #     network: { get_resource: my_network}
+  #     device_id: { get_resource: my_instance}
 `;
 
-/** heat template */
-// const YAML = `heat_template_version: 2016-10-14
-
-// description:
-//   # a description of the template
-
-// parameter_groups:
-//   # a declaration of input parameter groups and order
-
-// parameters:
-//   # declaration of input parameters
-//   user_name:
-//     type: string
-//     label: User Name
-//     description: User name to be configured for the application
-//     constraints:
-//       - length: { min: 6, max: 8 }
-//         description: User name must be between 6 and 8 characters
-//       - allowed_pattern: "[A-Z]+[a-zA-Z0-9]*"
-//         description: User name must start with an uppercase character
-//   port_number:
-//     type: number
-//     label: Port Number
-//     description: Port number to be configured for the web server
-//   instance_type:
-//     type: string
-//     label: Instance Type
-//     description: Instance type for compute instances
-//     constraints:
-//       - allowed_values:
-//         - m1.small
-//         - m1.medium
-//         - m1.large
-
-// resources:
-//   # declaration of template resources
-//   my_instance:
-//     type: OS::Nova::Server
-//     properties:
-//       flavor: m1.small
-//       image: F18-x86_64-cfntools
-
-//   server1:
-//     type: OS::Nova::Server
-//     depends_on: server2
-
-//   server2:
-//     type: OS::Nova::Server
-//     properties:
-//       # general properties ...
-//       user_data:
-//         str_replace:
-//           template: |
-//             #!/bin/bash
-//             echo "Hello world"
-//             echo "Setting MySQL root password"
-//             mysqladmin -u root password $db_rootpassword
-//             # do more things ...
-//           params:
-//             $db_rootpassword: { get_param: DBRootPassword }
-
-//   server3:
-//     type: OS::Nova::Server
-//     depends_on: [ server1, server2 ]
-
-// outputs:
-//   # declaration of output parametersoutputs:
-//   instance_ip:
-//     description: IP address of the deployed compute instance
-//     value: { get_attr: [my_instance, first_address] }
-
-// conditions:
-//   # declaration of conditions
-// `;
-
-const y = ref(YAML2);
+const y = ref(YAML);
 const templateData = ref<Record<any, any>>({});
 const json = computed(() => {
   return JSON.stringify(templateData.value, null, 2);
+});
+
+const resourceTypes = [
+  "OS::Nova::Server",
+  "OS::Cinder::Volume",
+  "OS::Neutron::Port",
+  "OS::Neutron::RouterInterface",
+  "OS::Neutron::Router",
+  "OS::Neutron::Subnet",
+  "OS::Neutron::Net",
+];
+
+/** defined resources on heat template */
+const resources = computed(() => {
+  return Object.keys(templateData.value?.resources ?? {});
 });
 
 const doYaml2Json = () => {
--- src/components/yaml/Property.vue
+++ src/components/yaml/Property.vue
@@ -1,20 +1,54 @@
 <template>
   <div>
+    <input
+      type="checkbox"
+      v-model="selectResourceDefined"
+      :disabled="disabled"
+    />
     <label>{{ property }}</label>
-    <input :type="inputType" disabled :value="inputVal" />
+    <template v-if="selectResourceDefined">
+      <select :value="inputVal?.get_resource ?? ''" :disabled="disabled">
+        <option value="">--Please choose an option--</option>
+        <option v-for="resource in resources" :key="resource" :value="resource">
+          {{ resource }}
+        </option>
+      </select>
+    </template>
+    <template v-else>
+      <input :type="inputType" :disabled="disabled" :value="inputVal" />
+    </template>
   </div>
 </template>
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref, watch, watchEffect } from "vue";
 
-const props = defineProps(["property", "properties", "type"]);
+const props = defineProps([
+  "property",
+  "properties",
+  "type",
+  "resources",
+  "disabled",
+]);
 
 const inputType = computed(() => {
   return props?.type ?? "string";
 });
 
+const selectResourceDefined = ref(false);
+
 const inputVal = computed(() => {
   return props.properties?.[props.property];
 });
+
+watchEffect(() => {
+  // selectResourceDefined.value =
+  //   typeof props.properties?.[props.property]?.get_resource !== "undefined";
+  selectResourceDefined.value =
+    typeof inputVal.value?.get_resource !== "undefined";
+});
+
+// watch(inputVal, () => {
+//   return typeof inputVal.value?.get_resource !== "undefined";
+// });
 </script>
 <style lang="scss" scoped></style>
